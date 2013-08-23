a6bba12670b3819e6d9401feb6546d2a69cf9a9d
afonso-praca <afonsoinfo@gmail.com>
2013-08-23 12:08:43 -0300

    adiciona provider

--- src/vtex-ng-pagination.js
+++ src/vtex-ng-pagination.js
@@ -1,110 +1,118 @@
-var mod;
+var mod, CONFIG;
 mod = angular.module('vtex-ng-pagination', []);
 
+CONFIG = {};
+
 mod.directive("pagination", function(){
-   return {
-     restrict: "E",
-     scope:{
-       callback: "&callback",
-       totalCount: "=total",
-       totalPages: "=pages"
-     },
-     templateUrl: 'modules/vtex-ng-pagination.html',
-     link: function ($scope) {
-       var callback = $scope.callback();
-       $scope.actions = {};
+	return {
+		restrict: "E",
+		scope:{
+			callback: "&callback",
+			totalCount: "=total",
+			totalPages: "=pages"
+		},
+		templateUrl: 'modules/vtex-ng-pagination.html',
+		link: function ($scope) {
+			var callback = $scope.callback();
+			$scope.actions = {};
 
-       // ORDER
-       $scope.sort = 'name';
-       $scope.sortType = 'ASC';
+			// ORDER
+			$scope.sort = 'name';
+			$scope.sortType = 'ASC';
 
-       // DISBALED BTS
-       $scope.disablePrevious = "disabled";
-       $scope.disableNext = "";
+			// DISBALED BTS
+			$scope.disablePrevious = "disabled";
+			$scope.disableNext = "";
 
-       // VARIAVEIS DE CONTROLE
-       $scope.numItemsPerPage = 15;
-       $scope.currentPage = 1;
+			// VARIAVEIS DE CONTROLE
+			$scope.numItemsPerPage = 15;
+			$scope.currentPage = 1;
 
-       // COMPUTED OBSERVABLES
-       $scope.$watch('totalPages', function(){
-         $scope.verifyBtns($scope.currentPage);
-       });
+			// COMPUTED OBSERVABLES
+			$scope.$watch('totalPages', function(){
+				$scope.verifyBtns($scope.currentPage);
+			});
 
-       $scope.$watch('currentPage', function(newValue){
-         if (newValue && (newValue <= $scope.totalPages) && (newValue > 0)){
-           $scope.actions.callback();
-         } else if (newValue && (newValue <= 0)){
-           $scope.currentPage = 1;
-         } else if (newValue && (newValue > $scope.totalPages)){
-           $scope.currentPage = $scope.totalPages ? $scope.totalPages : 1;
-         }
-         $scope.verifyBtns(newValue);
-       });
+			$scope.$watch('currentPage', function(newValue){
+				if (newValue && (newValue <= $scope.totalPages) && (newValue > 0)){
+					$scope.actions.callback();
+				} else if (newValue && (newValue <= 0)){
+					$scope.currentPage = 1;
+				} else if (newValue && (newValue > $scope.totalPages)){
+					$scope.currentPage = $scope.totalPages ? $scope.totalPages : 1;
+				}
+				$scope.verifyBtns(newValue);
+			});
 
-			 /** This function enable or disable the pagination buttons. */
-       $scope.verifyBtns = function(newValue){
-         if (newValue > 1 && newValue <= $scope.totalPages){
-           $scope.disablePrevious = "";
-         } else {
-           $scope.disablePrevious = "disabled";
-         }
+			$scope.verifyBtns = function(newValue){
+				if (newValue > 1 && newValue <= $scope.totalPages){
+					$scope.disablePrevious = "";
+				} else {
+					$scope.disablePrevious = "disabled";
+				}
 
-         if (newValue < $scope.totalPages){
-           $scope.disableNext = "";
-         } else {
-           $scope.disableNext = "disabled";
-         }
-       };
+				if (newValue < $scope.totalPages){
+					$scope.disableNext = "";
+				} else {
+					$scope.disableNext = "disabled";
+				}
+			};
 
-			 /** This function is called every time user wants to change page. */
-       $scope.changePage = function(direction){
-         if ((direction === "next") && ($scope.currentPage < $scope.totalPages)){
-           $scope.currentPage = Number($scope.currentPage)+1;
-         } else if ((direction === "prev") && ($scope.currentPage > 1) && ($scope.currentPage <= $scope.totalPages)){
-           $scope.currentPage = Number($scope.currentPage) - 1;
-         }
-       };
+			// UI ACTIONS
+			$scope.changePage = function(direction){
+				if ((direction === "next") && ($scope.currentPage < $scope.totalPages)){
+					$scope.currentPage = Number($scope.currentPage)+1;
+				} else if ((direction === "prev") && ($scope.currentPage > 1) && ($scope.currentPage <= $scope.totalPages)){
+					$scope.currentPage = Number($scope.currentPage) - 1;
+				}
+			};
 
-			 /** This function is called when user changes the number of items per page. */
-       $scope.setNumItemsPerPage = function(num){
-         $scope.numItemsPerPage = num;
-         $scope.currentPage = 1;
-         $scope.actions.callback();
-       };
+			$scope.setNumItemsPerPage = function(num){
+				$scope.numItemsPerPage = num;
+				$scope.currentPage = 1;
+				$scope.actions.callback();
+			};
 
-       $scope.actions.callback = function(){
-         if (callback){
-           var obj_callback = {
-             numItemsPerPage: $scope.numItemsPerPage,
-             currentPage: $scope.currentPage,
-             sort: $scope.sort,
-             sortType: $scope.sortType
-           };
-           callback(obj_callback);
-           $(".pagination input").trigger('blur');
-         }
-       };
+			$scope.actions.callback = function(){
+				if (callback){
+					var obj_callback = {
+						numItemsPerPage: $scope.numItemsPerPage,
+						currentPage: $scope.currentPage,
+						sort: $scope.sort,
+						sortType: $scope.sortType
+					};
+					callback(obj_callback);
+					$(".pagination input").trigger('blur');
+				}
+			};
 
-			 /** Adds Jquery listeners to DOM elements. */
-       $scope.actions.addHtmlListeners = function(){
-         $(".pagination input")
-           .off('mouseover mouseout blur')
-           .on('mouseover', function() {
-             $(this).tooltip({'trigger': 'manual'});
-             $(this).tooltip('show');
-           })
-           .on('mouseout', function() {
-             if (!$(this).is(':focus')) {
-               $(this).tooltip('hide');
-             }
-           })
-           .on('blur', function() {
-             $(this).tooltip('hide');
-           });
-       };
+			// VIEW ACTIONS
+			$scope.actions.addHtmlListeners = function(){
+				$(".pagination input")
+					.off('mouseover mouseout blur')
+					.on('mouseover', function() {
+						$(this).tooltip({'trigger': 'manual'});
+						$(this).tooltip('show');
+					})
+					.on('mouseout', function() {
+						if (!$(this).is(':focus')) {
+							$(this).tooltip('hide');
+						}
+					})
+					.on('blur', function() {
+						$(this).tooltip('hide');
+					});
+			};
 
-       $scope.actions.addHtmlListeners();
-     }
-   }
+			$scope.actions.addHtmlListeners();
+		}
+	}
+});
+
+mod.provider('vtex-ng-pagination', function(){
+	return {
+		$get: function(paginate){
+			return paginate;
+		}
+	}
 });
